d1a829b7d823344e033a6107835c7b896e1ec9ce
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-02 09:28:59 +0600

    navber add

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h3>This is header</h3>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Chef And Food House</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Blog</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </div>
     );
 };
